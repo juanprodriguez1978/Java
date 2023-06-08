e21b300f1ea4ef5e65b5a4ab366b6314f63ee1be
Juan Pablo Rodriguez <juanprodriguez.mail@gmail.com>
2023-06-08 11:34:38 -0300

    Ejemplos de Variables

---
+++ EjemplosVariables/src/EjemplosVariables.java
@@ -0,0 +1,13 @@
+public class EjemplosVariables {
+    public static void main(String[] args) {
+        int edad = 45;
+        long a = 4000000;
+        double b = 4.56 , c = 4.5;
+        String nombre = "Pedro";
+        int [] array = new int[5];
+
+        System.out.println(edad);
+        System.out.println("Mi nombre es: " + nombre);
+
+    }
+}
