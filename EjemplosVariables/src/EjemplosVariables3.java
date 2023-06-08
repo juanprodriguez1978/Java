de9e7909b648df6b25a11f522eb0d58773b7bd39
Juan Pablo Rodriguez <juanprodriguez.mail@gmail.com>
2023-06-08 13:39:00 -0300

    Ejemplos de Variables3

---
+++ EjemplosVariables/src/EjemplosVariables3.java
@@ -0,0 +1,9 @@
+public class EjemplosVariables2 {
+    public static void main(String[] args) {
+        int a = 10;
+        long b = 20;
+        byte c = 30;
+
+        System.out.println("La suma de los 3 numeros es igual a " + (a+b+c));
+    }
+}
